99822c49bd5149dc37acad7107e9a96fa33cb0be
lorae <stojanovic.lorae@gmail.com>
2025-06-12 14:55:04 -0400

    feat(run-regression): minimal working function example

--- kob/scripts/refactor/run-regression.R
+++ kob/scripts/refactor/run-regression.R
@@ -1,4 +1,25 @@
 # kob/scripts/refactor/run-regression.R
 # The purpose of this script is to run the regressions as the first stage of the KOB
 # decomposition.
+library("rlang")
 
+# TODO: add a se_style argument that specifies the way standard errors are to be
+# calculated.
+# TODO: add formula argument
+run_regression <- function(data, weights) {
+  weights_col <- eval_tidy(sym(weights), data)
+  
+  model <- lm(
+    formula = NUMPREC ~ HHINCOME_bucket + EDUC_bucket,
+    data = data,
+    weights = weights_col,
+    contrasts = list(
+      HHINCOME_bucket = "contr.treatment",
+      EDUC_bucket = "contr.treatment"
+    )
+  )
+  
+  print(model)
+}
+
+run_regression(data= input, weights = "PERWT")
