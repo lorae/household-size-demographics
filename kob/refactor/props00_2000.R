ef176f6b29e5dfc6eec5c48d11fb9ada40c591e2
lorae <stojanovic.lorae@gmail.com>
2025-06-18 10:58:22 -0400

    refactor: rename script for calculating 2000 proportions for regression 00

--- kob/refactor/props00_2000.R
+++ kob/refactor/props00_2000.R
@@ -1,4 +1,4 @@
-# kob/refactor/reg00_2000
+# kob/refactor/props00_2000
 # This script runs the props for regression 00 in 2000.
 
 # TODO: explore whether I can generate a survey design without pulling data into
@@ -29,4 +29,34 @@ library(furrr)
 # Read in the pre-subsetted survey
 tic("Read 2000 survey design as RDS")
 design_2000_survey <- readRDS("kob/throughput/design_2000_survey.rds")
+toc()
+
+# Calculate proportions
+prop_vars <- c(
+  "RACE_ETH_bucket", 
+  "AGE_bucket", 
+  "EDUC_bucket",
+  "INCTOT_cpiu_2010_bucket", 
+  "us_born", 
+  "tenure", 
+  "gender",
+  "cpuma"
+  )
+
+# Convert to one-sided formulas: ~varname
+formulas <- lapply(prop_vars, function(var) as.formula(paste0("~", var)))
+
+# Set up parallel plan (this will use available cores)
+options(future.globals.maxSize = 10 * 1024^3)
+plan(multicore)
+
+tic("Parallelized population proportions (forked)")
+props00_2000 <- future_map(formulas, ~svymean(.x, design_2000_survey))
+toc()
+
+names(props00_2000) <- prop_vars
+
+# Save results in throughput
+tic("Save props00_2000 to kob/throughput")
+saveRDS(props00_2000, file = "kob/throughput/props00_2000.rds")
 toc()
