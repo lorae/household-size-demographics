8f882a73b48d32bf6b4e1098bd6706bd0b7ae6eb
lorae <stojanovic.lorae@gmail.com>
2025-06-23 15:38:52 -0400

    refactor(benchmark): clarify deprecated and misleading comments, rename vars for clarity

--- kob/benchmark/create-benchmark-data.R
+++ kob/benchmark/create-benchmark-data.R
@@ -11,12 +11,12 @@ library(dplyr)
 
 #' all_exist
 #'
-#' Determines whether a step should run based on file existence.
-#' Returns TRUE if any file is missing, FALSE otherwise.
+#' Determines whether all specified file inputs in arguments exists.
+#' Returns TRUE all files exist, FALSE otherwise.
 #' TODO: this might be later replaced with `targets` package
 #'
 #' @param ... One or more file paths
-#' @return Logical scalar: TRUE if any file is missing, FALSE otherwise
+#' @return Logical scalar: TRUE if all files exist, FALSE otherwise
 all_exist <- function(...) {
   all(file.exists(...))
 }
@@ -39,10 +39,11 @@ all_exist <- function(...) {
 # - force: whether to overwrite cached results
 #
 # output:
-# - it will save the created benchmark to a directory called
+# - It saves benchmark to a directory called
 # `kob/cache/benchmark_sample_{year}_{n_strata}`
 # - There will be one file in that folder with the db and one with the tb
 # - within the db, the table name will be "ipums_sample"
+# - returns an invisible NULL if any of the defensive validation checks are failed
 create_benchmark_sample <- function(
     year = 2019,
     n_strata = 3,
@@ -51,11 +52,11 @@ create_benchmark_sample <- function(
     force = FALSE # TRUE will recalculate benchmark sample, even if it already exists in cache
 ) {
   # Path to the cache where the output will be stored
-  output_path = glue("kob/cache/benchmark_sample_{year}_{n_strata}") |> as.character()
-  output_tb <- glue("{output_path}/tb.rds") |> as.character()
-  output_db <- glue("{output_path}/db.duckdb") |> as.character()
+  output_dir = glue("kob/cache/benchmark_sample_{year}_{n_strata}") |> as.character()
+  output_tb_path <- glue("{output_dir}/tb.rds") |> as.character()
+  output_db_path <- glue("{output_dir}/db.duckdb") |> as.character()
   
-  outputs_exist <- all_exist(output_db, output_tb)
+  outputs_exist <- all_exist(output_db_path, output_tb_path)
   
   # Early break
   if (outputs_exist & !force) {
@@ -67,8 +68,7 @@ create_benchmark_sample <- function(
   message("⚙️  Generating benchmark files...")
   
   # Ensure the output_dir already exists. If it doesn't, create it.
-  # TODO: rename output_path to output_dir
-  dir.create(output_path, recursive = TRUE, showWarnings = FALSE)
+  dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
   
   
   # Connect to DB; assign alias to table
@@ -76,6 +76,8 @@ create_benchmark_sample <- function(
   ipums_db <- tbl(con, db_table_name)
 
   # Sample {`n_strata`} strata from {`year`} data
+  # TODO: defensive check: validate n_strata <= number of available strata in sample year
+  # TODO: defensive check: validate that {year} exists in data
   strata_sample <- ipums_db |> 
     filter(YEAR == year) |> 
     distinct(STRATA) |> 
@@ -83,25 +85,29 @@ create_benchmark_sample <- function(
     head(n_strata) |>
     collect()
   
-  # Create the sampled tb & write to `output_tb`
+  # Create the sampled tb & write to `output_tb_path`
   ipums_sample_tb <- ipums_db |> 
     filter(YEAR == year, STRATA %in% !!strata_sample$STRATA) |> 
     collect()
-  saveRDS(ipums_sample_tb, file = output_tb)
-  if(all_exist(output_tb)) {
-    message(glue("✅ Saved benchmark tb of {year} data with {n_strata} strata to {output_tb}."))
+  saveRDS(ipums_sample_tb, file = output_tb_path)
+  if(all_exist(output_tb_path)) {
+    message(glue("✅ Saved benchmark tb of {year} data with {n_strata} strata to {output_tb_path}."))
   } else {
     warning("❗ Benchmark tb save was unsuccessful.")
   }
   
-  # Write the sampled tb to `output_db`
-  output_db_con <- dbConnect(duckdb::duckdb(), output_db)
+  # Write the sampled tb to `output_db_path`
+  output_db_con <- dbConnect(duckdb::duckdb(), output_db_path)
   copy_to(output_db_con, ipums_sample_tb, "ipums_sample", overwrite = TRUE)
-  if(all_exist(output_db)) {
-    message(glue("✅ Saved benchmark db of {year} data with {n_strata} strata to {output_db}."))
+  if(all_exist(output_db_path)) {
+    message(glue("✅ Saved benchmark db of {year} data with {n_strata} strata to {output_db_path}."))
   } else {
     warning("❗ Benchmark db save was unsuccessful.")
   }
+  
+  # Clean up db connections
+  dbDisconnect(con, shutdown = TRUE)
+  dbDisconnect(output_db_con, shutdown = TRUE)
 }
 
 # Example usage (for dev only)
@@ -111,4 +117,13 @@ create_benchmark_sample(
   db_path = "data/db/ipums.duckdb",
   db_table_name = "ipums_processed",
   force = FALSE
+)
+
+# Example usage (for dev only)
+create_benchmark_sample(
+  year = 2019,
+  n_strata = 3,
+  db_path = "data/db/ipums.duckdb",
+  db_table_name = "ipums_processed",
+  force = TRUE
 )
