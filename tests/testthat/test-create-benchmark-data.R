42df8ed104701bb6817a041ffd53344ec4f298ea
lorae <stojanovic.lorae@gmail.com>
2025-06-23 16:34:01 -0400

    tests: add first unit test for create_benchmark_sample

--- tests/testthat/test-create-benchmark-data.R
+++ tests/testthat/test-create-benchmark-data.R
@@ -1,15 +1,37 @@
-# Config
+# tests/testthat/test-create-benchmark-data.R
+#
+# ----- Step 0: Config -----
+library(rprojroot)
+
+# Ensure working directory is project root
+root <- find_root(is_rstudio_project)
+setwd(root)
+
+# Source the functions we're testing
 source("kob/benchmark/create-benchmark-data.R")
 
-# Example usages (for dev only) - move to unit tests
-# Expect to not execute; return early due to cached results
-create_benchmark_sample(
-  year = 2019,
-  n_strata = 3,
-  db_path = "data/db/ipums.duckdb",
-  db_table_name = "ipums_processed",
-  force = FALSE
-)
+# ----- Step 1: Run the tests! -----
+test_that("create_benchmark_sample will create new dir if output_dir doesn't exist", {
+  # First, delete this dir if it exists
+  unlink("tests/testthat/test-create-benchmark-sample", recursive = TRUE, force = TRUE)
+  
+  # Run create_benchmark_sample, which should create the dir and deposit files
+  create_benchmark_sample(
+    year = 2019,
+    n_strata = 3,
+    db_path = "data/db/ipums.duckdb",
+    db_table_name = "ipums_processed",
+    output_dir = "tests/testthat/test-create-benchmark-sample",
+    force = FALSE
+  )
+  
+  # Confirm expected output files exist
+  expect_true(all_exist(
+    "tests/testthat/test-create-benchmark-sample/benchmark_sample_2019_3/db.duckdb",
+    "tests/testthat/test-create-benchmark-sample/benchmark_sample_2019_3/tb.rds"
+    )
+  )
+})
 
 # Expect to execute due to force = TRUE arg
 create_benchmark_sample(
