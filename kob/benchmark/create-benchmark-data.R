cb74e94f3f6fe12d55171f4bf9c6e0f63ed1d2f7
lorae <stojanovic.lorae@gmail.com>
2025-06-23 12:43:39 -0400

    feat(benchmark): define cache output paths within create_benchmark_sample()

--- kob/benchmark/create-benchmark-data.R
+++ kob/benchmark/create-benchmark-data.R
@@ -3,6 +3,11 @@
 # The purpose of the script is to modularize the creation of a benchmark sample
 # for use in tests of jobs and other functions.
 #
+# ----- Step 0: Config
+
+library(glue)
+
+#
 # create_benchmark_sample
 # input:
 # - args given below
@@ -10,17 +15,23 @@
 # - it will save the created benchmark to a directory called
 # `kob/cache/benchmark_sample_{year}_{n_strata}`
 # - There will be one file in that folder with the db and one with the tb
+# - within the db, the table name will be "ipums_sample"
 create_benchmark_sample <- function(
     year = 2019,
     n_strata = 3,
-    db_path = "data/db/ipums.duckdb",
-    benchmark_db_path = "data/db/benchmark.duckdb",
-    db_table_name = "ipums_processed",
-    benchmark_db_table_name = "ipums_sample",
+    db_path = "data/db/ipums.duckdb", # The name of the DB with the source data
+    db_table_name = "ipums_processed", # Name of the table within the DB of source data
     force = FALSE # TRUE will recalculate benchmark sample, even if it already exists in cache
 ) {
-  # Create output_file variable which is just the path to the cache where the output 
-  # will be stored
+  # Path to the cache where the output will be stored
+  output_path = glue("kob/cache/benchmark_sample_{year}_{n_strata}")
+  output_tb <- glue("{output_path}/tb.rds")
+  output_db <- glue("{output_path}/db.duckdb")
+  
+  print(output_path)
+  print(output_tb)
+  print(output_db)
+  
   # Check if both the db and dbs with expected file names exist inthe directory. Use
   # this info to assign output_dne = {t, f} where dne stands for does not exist.
   # assign true if one or both files are missing. False if everything that should be 
@@ -38,5 +49,13 @@ create_benchmark_sample <- function(
   # Save them
   
   # unit test comparing them can be external.
-  
 }
+
+# Example usage (for dev only)
+create_benchmark_sample(
+  year = 2019,
+  n_strata = 3,
+  db_path = "data/db/ipums.duckdb",
+  db_table_name = "ipums_processed",
+  force = FALSE
+)
