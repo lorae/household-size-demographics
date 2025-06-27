459c102bb879a87ace12d17c99a6526d022b7ae1
lorae <stojanovic.lorae@gmail.com>
2025-06-27 12:19:34 -0400

    feat: create an oaxaca result - doesn't match official Will have to investigate why

--- kob/deprecated-scripts/kob.R
+++ kob/deprecated-scripts/kob.R
@@ -1,3 +1,9 @@
+# The purpose of this script is to manually produce a kob analysis that exactly
+# matches the results of the oaxaca function. Over time this script will be gradually
+# refactored and functionalized until it is a fully validated working function that
+# can be applied to the outputs of the coefXX_YYYY_*.R and propsXX_YYYY.R scripts.
+# Everything aside from SEs will be validated.
+
 # ----- STEP 0: Configuration ----- #
 
 library(tibble)
@@ -7,12 +13,15 @@ library(purrr)
 library(tidyr)
 library(stringr)
 
+devtools::load_all("../dataduck")
+
 # Load synthetic data
 load_path <- "kob/synthetic-data"
 c_only <- readRDS(paste0(load_path, "/c-only.rds"))
 e_only <- readRDS(paste0(load_path, "/e-only.rds"))
 u_only <- readRDS(paste0(load_path, "/u-only.rds"))
 
+# TODO: if needed, move this function elsewhere to declutter the script
 # Function for one hot encoding `varlist`categorical variables in `data`
 one_hot_encode <- function(data, varlist, prefixes) {
   stopifnot(length(varlist) == length(prefixes))  # sanity check
@@ -68,11 +77,11 @@ c_only <- c_only |>
 # Compute linear regression results for 2000 and 2019
 model_2000 <- lm(data = c_only |> filter(year == 2000),
                  weights = PERWT,
-                 formula = NUMPREC ~ -1 + EDUC_bucket + -1 + HHINCOME_bucket)
+                 formula = NUMPREC ~ EDUC_bucket + HHINCOME_bucket)
 
 model_2019 <- lm(data = c_only |> filter(year == 2019),
                  weights = PERWT,
-                 formula = NUMPREC ~ -1 + EDUC_bucket + -1 + HHINCOME_bucket)
+                 formula = NUMPREC ~ EDUC_bucket + HHINCOME_bucket)
 
 coef_df <- full_join(
   enframe(model_2000$coefficients, name = "name", value = "mean_2000"),
@@ -121,10 +130,66 @@ non_intercepts <- coef_df |>
 coef_df <- bind_rows(intercept_row, non_intercepts)
 
 # Compute weighted counts for 2000 and 2019
-weighted_2000 <- c_only |> filter(year == 2000) |>
-  group_by(EDUC_bucket, HHINCOME_bucket) %>%
-  summarise(weighted_count_2000 = sum(PERWT), .groups = "drop")
+# simulate what I'll get from propsXX_YYYY.R. Why not actually run this script
+# on the output of that, insted of make a "fake" output? I want to test this as
+# a proof-of-concept. Once I show it works, I'll incorporate in the actual props 
+# and coef code.
+get_prop_table <- function(year) {
+  hhinc_tbl <- 
+    crosstab_percent(
+      data = c_only |> filter(year == year),
+      wt_col = "PERWT",
+      group_by = c("HHINCOME_bucket"),
+      percent_group_by = c(),
+      every_combo = TRUE
+    ) |>
+    rename(value = HHINCOME_bucket) |>
+    mutate(varname = "HHINCOME_bucket")
 
-weighted_2019 <- c_only |> filter(year == 2019) %>%
-  group_by(EDUC_bucket, HHINCOME_bucket) %>%
-  summarise(weighted_count_2019 = sum(PERWT), .groups = "drop")
+  educ_tbl <- 
+    crosstab_percent(
+      data = c_only |> filter(year == year),
+      wt_col = "PERWT",
+      group_by = c("EDUC_bucket"),
+      percent_group_by = c(),
+      every_combo = TRUE
+    ) |>
+      rename(value = EDUC_bucket) |>
+      mutate(varname = "EDUC_bucket")
+  
+  output <- bind_rows(hhinc_tbl, educ_tbl) |>
+    mutate(prop = percent/100) |>
+    select(-percent, -weighted_count, -count) |>
+    rename(!!paste0("prop_", year) := prop)
+  
+  return(output)
+}
+
+prop_2000 <- get_prop_table(2000)
+prop_2019 <- get_prop_table(2019)
+
+### Now we combine the prop tables with the coef table to get a combined, ready for
+# KOB, table
+kob <-
+  left_join(coef_df, prop_2000, by = c("varname", "value")) %>%
+  left_join(., prop_2019, by = c("varname", "value"))
+kob_analysis
+
+## Now let's do the KOB
+kob <- kob |>
+  mutate(
+    u = case_when(
+      name == "(Intercept)" ~ mean_2019 - mean_2000,
+      TRUE ~ NA_real_
+    ),
+    c = mean_2019 - mean_2000*prop_2000,
+    e = mean_2019*(prop_2019 - prop_2000)
+  )
+
+u = sum(kob$u, na.rm = TRUE)
+c = sum(kob$c, na.rm = TRUE)
+e = sum(kob$e, na.rm = TRUE)
+u + c + e
+
+c_only_oaxaca$threefold$overall
+
