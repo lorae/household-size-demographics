f3e67a02563117d6bc2e2e9c037ea4be0fc01a34
lorae <stojanovic.lorae@gmail.com>
2025-07-02 09:29:44 -0400

    feat(benchmark): add matrix-based vs svyglm-based test for regression coefs and SEs, with exact match expected Test appears to pass with tolerance 1e-10 on an initial run of a 3-stratum sample

---
+++ kob/benchmark/matrix-vs-survey-exact.R
@@ -0,0 +1,117 @@
+# kob/benchmark/matrix-vs-survey-exact.R
+cat("
+This script benchmarks whether the custom dataduck matrix-based regression and 
+successive differences replication on 2019 data exactly reproduces the point 
+estimates and standard errors from svyglm. It assumes that the replicate weights 
+may contain negative values and does NOT modify them. This tests whether the matrix 
+solver is a true drop-in replacement for survey design-based inference.
+\n")
+
+# ----- Step 0: User settings
+# Define tolerance level of tests (two numbers count as equal if they are within
+# `tol` of each other)
+tol <- 1e-10
+
+# Define number of strata to pull from 2019 data. Total data contains 2000 strata.
+# 3 is the smallest I'd recommend; 200 is the largest I'd recommend for a run that
+# doesn't take longer than 30 minutes.
+n_strata <- 3
+
+# Define regression formula
+formula <- BEDROOMS ~ -1 + 
+  RACE_ETH_bucket +
+  AGE_bucket +
+  EDUC_bucket +
+  INCTOT_cpiu_2010_bucket +
+  us_born +
+  tenure +
+  gender +
+  cpuma
+
+# ----- Step 1: Config ----- #
+
+library(survey)
+library(tictoc)
+library(dplyr)
+library(glue)
+library(purrr)
+library(devtools)
+
+# dataduck internal package & helper scripts
+# TODO: will someday be added to dataduck
+load_all("../dataduck")
+source("kob/benchmark/create-benchmark-data.R")
+source("kob/benchmark/regression-backends.R")
+
+# ----- step 1: Load and prepare sample ----- #
+create_benchmark_sample(
+  year = 2019,
+  n_strata = n_strata,
+  db_path = "data/db/ipums.duckdb",
+  db_table_name = "ipums_processed",
+  force = FALSE
+)
+
+ipums_2019_sample_tb <- readRDS(glue("kob/cache/benchmark_sample_2019_{n_strata}/tb.rds"))
+filtered_tb <- ipums_2019_sample_tb |> filter(GQ %in% c(0, 1, 2))
+
+# ----- step 2: Compute benchmark model using svyglm ----- #
+tic("create survey design")
+design_2019_expected <- svrepdesign(
+  weights = ~PERWT,
+  repweights = "REPWTP[0-9]+",
+  type = "Fay",
+  rho = 0.5,
+  mse = TRUE,
+  data = ipums_2019_sample_tb
+) |> subset(GQ %in% c(0, 1, 2))
+toc()
+
+tic("fit svyglm")
+model_expected <- svyglm(formula, design = design_2019_expected)
+toc()
+
+expected_coefs <- tibble(
+  term = names(model_expected$coefficients),
+  estimate = as.numeric(model_expected$coefficients)
+) |> arrange(term)
+
+expected_ses <- summary(model_expected)$coefficients[, "Std. Error"] |> unname()
+
+# ----- step 3: Run matrix regression and compare results ----- #
+
+model_actual <- dataduck_reg_matrix(
+  data = filtered_tb,
+  wt_col = "PERWT",
+  formula = formula
+)
+
+stopifnot(all.equal(model_actual$estimate, expected_coefs$estimate, tolerance = tol))
+message(glue("\u2705 Coefficients match within {tol}."))
+
+# ----- step 4: Bootstrap SEs using unmodified replicate weights ----- #
+
+input_bootstrap <- bootstrap_replicates(
+  data = filtered_tb,
+  f = dataduck_reg_matrix,
+  wt_col = "PERWT",
+  repwt_cols = paste0("REPWTP", 1:80),
+  id_cols = "term",
+  formula = formula
+)
+
+model_output <- se_from_bootstrap(
+  bootstrap = input_bootstrap,
+  constant = 4 / 80,
+  se_cols = c("estimate")
+)
+
+actual_ses <- model_output$se_estimate
+
+all_equal_helper <- function(x, y, tol) {
+  if (length(x) != length(y)) return(FALSE)
+  all(abs(sort(x) - sort(y)) < tol)
+}
+
+stopifnot(all_equal_helper(actual_ses, expected_ses, tol = tol))
+message(glue("\u2705 Standard errors match within {tol}."))
