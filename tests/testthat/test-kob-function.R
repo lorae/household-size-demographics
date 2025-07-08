7663b8123c5828d8e124da2eb2c807f54d112d96
Lorae <10428186+lorae@users.noreply.github.com>
2025-07-08 14:36:13 -0400

    tests(kob): introduce intercept input and expected dfs

--- tests/testthat/test-kob-function.R
+++ tests/testthat/test-kob-function.R
@@ -16,7 +16,7 @@ setwd(root)
 source("kob/scripts/kob-function.R")
 
 # ----- Step 1: Create a dummy kob_input -----
-kob_input_test <- tibble(
+kob_input <- tibble(
   term = c("foo", "bar", "baz"),
   coef_2000 = c(1.0, 2.0, 3.0),
   coef_2000_se = c(0.1, 0.2, 0.3),
@@ -27,3 +27,37 @@ kob_input_test <- tibble(
   prop_2019 = c(0.25, 0.35, 0.4),
   prop_2019_se = c(0.015, 0.025, 0.02)
 )
+
+kob_input_intercept <- tibble(
+  term = c("(Intercept)", "foo", "bar", "baz"),
+  coef_2000 = c(4.0, 1.0, 2.0, 3.0),
+  coef_2000_se = c(0.4, 0.1, 0.2, 0.3),
+  coef_2019 = c(4.5, 1.5, 2.5, 2.7),
+  coef_2019_se = c(0.5, 0.15, 0.25, 0.35),
+  prop_2000 = c(NA, 0.2, 0.3, 0.5),
+  prop_2000_se = c(NA, 0.01, 0.02, 0.03),
+  prop_2019 = c(NA, 0.25, 0.35, 0.4),
+  prop_2019_se = c(NA, 0.015, 0.025, 0.02)
+)
+
+# TODO: fill in with actual values
+kob_expected <- kob_input |>
+  mutate(
+    u = c(NA_real_, NA_real_, NA_real_),
+    u_se = c(NA_real_, NA_real_, NA_real_),
+    e = c(1, 2, 3),
+    e_se = c(1, 2, 3),
+    c = c(1, 2, 3),
+    c_se = c(1, 2, 3)
+  )
+
+# TODO: fill in with actual values
+kob_expected_intercept <- kob_input_intercept |>
+  mutate(
+    u = c(0.5, NA_real_, NA_real_, NA_real_),
+    u_se = c(100, NA_real_, NA_real_, NA_real_),
+    e = c(NA_real_, 1, 2, 3),
+    e_se = c(NA_real_, 1, 2, 3),
+    c = c(NA_real_, 1, 2, 3),
+    c_se = c(NA_real_, 1, 2, 3)
+  )
