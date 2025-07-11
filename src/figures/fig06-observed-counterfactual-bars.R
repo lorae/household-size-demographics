80a0c25920c953b71ec0f7f9cd3b8afeea571bd1
lorae <stojanovic.lorae@gmail.com>
2025-07-11 11:24:42 -0400

    feat: create dir for figures; write header for fig06 script

---
+++ src/figures/fig06-observed-counterfactual-bars.R
@@ -0,0 +1,5 @@
+# #src/figures/fig06-observed-counterfactual-bars.R
+# The purpose of this script is produce bar charts that show 2000 observed, 2019
+# observed, and 2019 and expected (counterfactual) outcomes of number of people 
+# in a household, number bedrooms in a household, and bedroom crowding.
+# Also an appendix version that does the same set of 3 bars by room.
