cf138c5db5954f5b1b34c7a260c950466906d606
lorae <stojanovic.lorae@gmail.com>
2025-07-14 15:17:24 -0400

    feat(fig01): refine aesthetics and simplify code

--- src/figures/fig01-hhsize-race-year-bars.R
+++ src/figures/fig01-hhsize-race-year-bars.R
@@ -2,9 +2,8 @@
 #
 # Produce bar charts showing hhsize by race/ethnicity and year (paired bars)
 #
-# Input: throughput/kob_output.rds
-# Output: output/figures/fig01-zomthing.png, 
-#         output/figures/fig01-tbd.png
+# Input: data/db/ipums.duckdb
+# Output: output/figures/fig01-hhsize-race-year-bars.png
 #
 # TODO: write unit tests for functions
 
@@ -24,16 +23,7 @@ source("src/utils/aggregation-tools.R")
 con <- dbConnect(duckdb::duckdb(), "data/db/ipums.duckdb")
 ipums_db <- tbl(con, "ipums_processed")
 
-# ----- Step 4a: RESULTS - Household size in 2000 and 2019 ----- #
-# TODO: export as table to Shiny app
-tabulate_summary_2year(data = ipums_db, years = c(2000,2019), group_by = c())
-
-# ----- Step 4b: RESULTS - Household size in 2000 and 2019 by race/ethnicity ----- #
-# TODO: export as table to Shiny app
-tabulate_summary_2year(data = ipums_db, years = c(2000,2019), group_by = "RACE_ETH_bucket")
-
-### # FIGURE 1: Create the bar plot with side-by-side bars for 2000 and 2019
-# Create data for bar plot
+# ----- Step 3: Make plots ----- #
 race_summary_2000 <- tabulate_summary(data = ipums_db, year = 2000, group_by = "RACE_ETH_bucket") |> mutate(year = 2000)
 race_summary_2019 <- tabulate_summary(data = ipums_db, year = 2019, group_by = "RACE_ETH_bucket") |> mutate(year = 2019)
 race_summary <- union_all(race_summary_2000, race_summary_2019) # Row bind the tables
@@ -61,5 +51,9 @@ fig01 <- ggplot(race_summary, aes(x = subgroup, y = hhsize, fill = factor(year))
     plot.margin = margin(t = 10, r = 10, b = 0, l = 10)
   )
 
-# Save the plot
-# ggsave("results/fig01.png", plot = fig01, width = 6.5, height = 3.5, dpi = 500)
+# ----- Step 4: Save plots ----- #
+ggsave(
+  "output/figures/fig01-hhsize-race-year-bars.png", 
+  plot = fig01, 
+  width = 3000, height = 2400, units = "px", dpi = 400
+)
