f9a49f57a2934bf6a2347b0bbef03a5654586e39
lorae <stojanovic.lorae@gmail.com>
2025-07-15 17:48:15 -0400

    refactor(fig02): move plotting function to step 1

--- src/figures/fig02-hhsize-race-hist.R
+++ src/figures/fig02-hhsize-race-hist.R
@@ -15,43 +15,7 @@ library("glue")
 devtools::load_all("../dataduck") # The crosstab_count() function used here is defined in dataduck
 
 # ----- Step 1: Define functions ----- #
-
-
-# ----- Step 2: Import and wrangle data ----- #
-con <- dbConnect(duckdb::duckdb(), "data/db/ipums.duckdb")
-ipums_db <- tbl(con, "ipums_processed")
-
-# 2019 estimate of non-group-quarters population in household of X size and Y race/ethnicity
-hhsize_race_2019 <- crosstab_count(
-  data = ipums_db |> filter(YEAR == 2019 & GQ %in% c(0,1,2)),
-  wt_col = "PERWT",
-  group_by = c("NUMPREC", "RACE_ETH_bucket"),
-  every_combo = TRUE
-) |> collect() |> arrange(RACE_ETH_bucket, NUMPREC)
-
-# ----- Step 3: Make plots ----- #
-# Choose a max NUMPREC (household size) to display in histogram
-topcode_hhsize <- 10 
-
-# Topcode the table
-fig02_data <- hhsize_race_2019 |>
-  mutate(NUMPREC = if_else(NUMPREC >= topcode_hhsize, topcode_hhsize, NUMPREC)) |>
-  group_by(RACE_ETH_bucket, NUMPREC) |>
-  summarize(
-    weighted_count = sum(weighted_count),
-    count = sum(count),
-    .groups = "drop"
-  ) |>
-  arrange(RACE_ETH_bucket, NUMPREC) |>
-  # Add frequencies for each NUMPREC value within RACE_ETH_bucket subpopulations
-  group_by(RACE_ETH_bucket) |>
-  mutate(freq = weighted_count / sum(weighted_count)) |>
-  ungroup()
-
-# Plot!!!
-filtered_data <- fig02_data |> filter(RACE_ETH_bucket == "Black")
-main_color <- "steelblue"
-
+# Plotting function for one histogram
 plot_hhsize_histogram <- function(data = fig02_data,
                                   main_color = "steelblue",
                                   title = NULL,
@@ -96,11 +60,44 @@ plot_hhsize_histogram <- function(data = fig02_data,
   return(p)
 }
 
+# ----- Step 2: Import and wrangle data ----- #
+con <- dbConnect(duckdb::duckdb(), "data/db/ipums.duckdb")
+ipums_db <- tbl(con, "ipums_processed")
+
+# 2019 estimate of non-group-quarters population in household of X size and Y race/ethnicity
+hhsize_race_2019 <- crosstab_count(
+  data = ipums_db |> filter(YEAR == 2019 & GQ %in% c(0,1,2)),
+  wt_col = "PERWT",
+  group_by = c("NUMPREC", "RACE_ETH_bucket"),
+  every_combo = TRUE
+) |> collect() |> arrange(RACE_ETH_bucket, NUMPREC)
+
+# ----- Step 3: Make plots ----- #
+# Choose a max NUMPREC (household size) to display in histogram
+topcode_hhsize <- 10 
+
+# Topcode the table
+fig02_data <- hhsize_race_2019 |>
+  mutate(NUMPREC = if_else(NUMPREC >= topcode_hhsize, topcode_hhsize, NUMPREC)) |>
+  group_by(RACE_ETH_bucket, NUMPREC) |>
+  summarize(
+    weighted_count = sum(weighted_count),
+    count = sum(count),
+    .groups = "drop"
+  ) |>
+  arrange(RACE_ETH_bucket, NUMPREC) |>
+  # Add frequencies for each NUMPREC value within RACE_ETH_bucket subpopulations
+  group_by(RACE_ETH_bucket) |>
+  mutate(freq = weighted_count / sum(weighted_count)) |>
+  ungroup()
+
+# Plot
+main_color <- "steelblue"
 
 plot_hhsize_histogram(
-  data = fig02_data |> filter(RACE_ETH_bucket == "Black"),
-  main_color = "tomato",
-  title = "Black Households",
+  data = fig02_data |> filter(RACE_ETH_bucket == "White"),
+  main_color = main_color,
+  title = "Black",
   xtitle = FALSE,
   ytitle = TRUE
 )
