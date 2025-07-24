c3dd087ab465f14c3a597440114b2f1be623f67c
lorae <stojanovic.lorae@gmail.com>
2025-07-24 11:30:06 -0400

    refactor: move state shapefile cleaning to new src/utils/mapping-tools.R script

--- src/scripts/fine-grained-counterfactual-regional.R
+++ src/scripts/fine-grained-counterfactual-regional.R
@@ -75,6 +75,7 @@ cf_hhsize <- counterfactual_components(
   p1_data = p1_sample,
   outcome = "NUMPREC"
 ) |>
+  # TODO: rename "State" upstream to "state"
   left_join(cpuma_state_cross, by = "CPUMA0010") # Adds "State" as a col
   
 hhsize_state_cf <- summarize_counterfactual(
@@ -97,54 +98,12 @@ hhsize_state_summary <- hhsize_state_cf$by |> select(
   diff
 )
 
-
-##############################################
-#vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv#
-##############################################
-### FIGURE 4: unexplained diff by state
-# Creates a 2x2 matrix representing the rotation transformation relative to angle
-# a
-rot <- function(a) {
-  matrix(c(cos(a), sin(a), -sin(a), cos(a)), 2, 2)
-}
-
-# Moves a state according to custom specifications
-transform_state <- function(
-    df, 
-    state_fp, 
-    rotation_angle, 
-    scale_factor, 
-    shift_coords
-) {
-  state <- df %>% filter(STATEFIP == state_fp)
-  state_geom <- st_geometry(state)
-  state_centroid <- st_centroid(st_union(state_geom))
-  rotated_geom <- (state_geom - state_centroid) * rot(rotation_angle * pi / 180) / scale_factor + state_centroid + shift_coords
-  state %>% st_set_geometry(rotated_geom) %>% st_set_crs(st_crs(df))
-}
-
-# Load shapefiles. Data is unzipped from https://www.weather.gov/gis/USStates
-# Unfortunately, the Census shapefiles I tried to download all had strange shapes
-# because they included water bodies.
-state_sf <- st_read("data/s_05mr24/s_05mr24.shp") |>
-  rename(STATEFIP = FIPS) |> # For consistency with household size data
-  filter(!STATEFIP %in% c('60', '64', '66', '68', '69', '70', '72', '78')) |># Remove excluded states, like Puerto Rico
-  st_transform(crs = "+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs") |>
-  mutate(geometry = st_simplify(geometry, dTolerance =  5000))  # Simplify shapes
-
-
-# Rotate and move Alaska and Hawaii to fit on map
-alaska <- transform_state(state_sf, "02", -39, 2.3, c(1000000, -5000000))
-hawaii <- transform_state(state_sf, "15", -35, 1, c(5200000, -1400000))
-
-# Final map after transforming non-contiguous states
-state_sf_final <- state_sf %>%
-  filter(!STATEFIP %in% c("02", "15")) |>
-  bind_rows(alaska, hawaii)
+# Do something to source src/utils/mapping-tools.R
+state_sf <- readRDS("throughput/state_shapefiles.rds")
 
 # Join the state data with household size differences
-state_sf_hhsize <- state_sf_final |>
-  left_join(hhsize_state_summary, by = "STATEFIP")
+state_sf_hhsize <- state_sf |>
+  left_join(hhsize_state_summary, by = "State")
 
 # Choropleth map (color version)
 fig04 <- ggplot(state_sf_hhsize) + 
