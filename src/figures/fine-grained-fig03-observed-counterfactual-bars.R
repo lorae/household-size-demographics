ee1be8320f8d3fb8bb0022118a0f14c2d6dd2973
lorae <stojanovic.lorae@gmail.com>
2025-07-24 15:07:46 -0400

    refactor: move fig06 plotting function to plotting-tools and rename generically

---
+++ src/figures/fine-grained-fig03-observed-counterfactual-bars.R
@@ -0,0 +1,7 @@
+# src/figures/fine-grained-fig03-observed-counterfactual-bars.R
+# Produce bar charts showing 2000 observed, 2019 observed, and 2019 and expected 
+# (counterfactual) outcomes.
+#
+# Input: TBD
+# Output: TBD
+
