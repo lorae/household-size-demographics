8ad23afff271408d022552afe819ed655eaf9603
lorae <stojanovic.lorae@gmail.com>
2025-07-28 18:45:43 -0400

    feat: graph the McClure-Schwartz surplus

---
+++ src/figures/fine-grained-fig05-m-s-surplus-state-map.R
@@ -0,0 +1,40 @@
+# ----- Step 0: Load required packages ----- #
+library("dplyr")
+library("duckdb")
+library("stringr")
+library("tidyr")
+library("purrr")
+library("glue")
+library("readxl")
+library("ggplot2")
+library("base64enc")
+library("sf")
+options(scipen = 999)
+
+# ----- Step 1: Source helper functions ----- #
+
+devtools::load_all("../dataduck")
+source("src/utils/counterfactual-tools.R") # Includes function for counterfactual calculation
+load("data/helpers/cpuma-state-cross.rda") # Crosswalks CPUMA0010 to state
+load("data/helpers/state-pop-growth.rda") # May be deprecated
+
+# ----- Step 2: Import data ----- #
+state_sf <- readRDS("throughput/state_shapefiles.rds") # One shapefile row per state
+ms_state_summary <- readRDS("throughput/mcclure-schwartz-analysis-state.rds") |>
+  filter(region != "United States", region != "Puerto Rico") |>
+  rename(State = region)
+
+# ----- Step 4: Map ----- #
+# --- Fig04a: hhsize diff by state ----
+state_sf_hhsize <- state_sf |>
+  left_join(ms_state_summary, by = "State")
+
+fig05 <- ggplot(state_sf_hhsize) + 
+  geom_sf(aes(geometry = geometry, fill = unit_surplus_2000_2020), color = "black", size = 0.5) +
+  scale_fill_gradient2(
+    name = "Housing surplus, number of units",
+    low = "#F94144", mid = "white", high = "blue", midpoint = 0,
+    breaks = seq(from = -0, to = 350000, by = 50000)
+  ) +
+  theme_void()
+fig05
