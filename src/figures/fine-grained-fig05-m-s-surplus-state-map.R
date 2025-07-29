c4b1cd9a236d917329163e844242a52e75e1c60f
lorae <stojanovic.lorae@gmail.com>
2025-07-29 11:51:21 -0400

    feat: add mcclure-schwartz map

--- src/figures/fine-grained-fig05-m-s-surplus-state-map.R
+++ src/figures/fine-grained-fig05-m-s-surplus-state-map.R
@@ -22,14 +22,19 @@ load("data/helpers/state-pop-growth.rda") # May be deprecated
 state_sf <- readRDS("throughput/state_shapefiles.rds") # One shapefile row per state
 ms_state_summary <- readRDS("throughput/mcclure-schwartz-analysis-state.rds") |>
   filter(region != "United States", region != "Puerto Rico") |>
-  rename(State = region)
+  rename(State = region) |>
+  mutate(
+    scaled_surplus = unit_surplus_2000_2020 / units_2020
+  )
+
+write.csv(ms_state_summary, "mcclure-scwartz-state.csv")
 
 # ----- Step 4: Map ----- #
 # --- Fig04a: hhsize diff by state ----
 state_sf_hhsize <- state_sf |>
   left_join(ms_state_summary, by = "State")
 
-fig05 <- ggplot(state_sf_hhsize) + 
+fig05a <- ggplot(state_sf_hhsize) + 
   geom_sf(aes(geometry = geometry, fill = unit_surplus_2000_2020), color = "black", size = 0.5) +
   scale_fill_gradient2(
     name = "Housing surplus, number of units",
@@ -37,4 +42,14 @@ fig05 <- ggplot(state_sf_hhsize) +
     breaks = seq(from = -0, to = 350000, by = 50000)
   ) +
   theme_void()
-fig05
+fig05a
+
+fig05b <- ggplot(state_sf_hhsize) + 
+  geom_sf(aes(geometry = geometry, fill = scaled_surplus), color = "black", size = 0.5) +
+  scale_fill_gradient2(
+    name = "Housing surplus, number of units",
+    low = "#F94144", mid = "white", high = "blue", midpoint = 0,
+    breaks = seq(from = 0, to = 0.06, by = 0.01)
+  ) +
+  theme_void()
+fig05b
