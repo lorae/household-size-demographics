d6f4eef8de7f36020b1a571e5bd97065145e3e5f
lorae <stojanovic.lorae@gmail.com>
2025-08-04 11:40:45 -0400

    refactor(standardize_coefs): move this wrapper function into regression-postprocess-tools.R

--- src/scripts/kob-build-input.R
+++ src/scripts/kob-build-input.R
@@ -142,43 +142,7 @@ read_coefs_2019 <- function(path) {
   return(output)
 }
 
-# Define helper function to standardize regression coefficients
 
-
-standardize_coefs <- function(
-    reg_data,
-    adjust_by = adjust_by
-) {
-  # Varnames fed into split_term_column() function, below
-  varnames_dict <- c(
-    "RACE_ETH_bucket",
-    "AGE_bucket",
-    "EDUC_bucket",
-    "INCTOT_cpiu_2010_bucket",
-    "us_born",
-    "gender",
-    "tenure",
-    "cpuma"
-  )
-  
-  output <- reg_data |>
-    split_term_column(varnames = varnames_dict) |>
-    # TODO: add unit test
-    add_intercept_v2(
-      variable = "RACE_ETH_bucket", # Variable to draw intercept from
-      reference_value = "White", # value of variable that will become intercept
-      coef_col = "coef_2000",
-      se_col = "coef_2000_se"
-    ) |>
-    complete_implicit_zeros(
-      adjust_by = adjust_by,
-      coef_col = "estimate",
-      se_col = NULL
-    ) 
-  
-  # TODO: this is incomplete and doesn't work. resume work on it
-  return(output)
-}
 
 # Define function to pull coefficient data from both years and combine with props
 # data
--- src/utils/regression-postprocess-tools.R
+++ src/utils/regression-postprocess-tools.R
@@ -354,3 +354,42 @@ complete_implicit_zeros <- function(
   
   return(reg_output_full)
 }
+
+
+
+# Define helper function to standardize regression coefficients in a single regression
+# function
+standardize_coefs <- function(
+    reg_data,
+    adjust_by = adjust_by
+) {
+  # Varnames fed into split_term_column() function, below
+  varnames_dict <- c(
+    "RACE_ETH_bucket",
+    "AGE_bucket",
+    "EDUC_bucket",
+    "INCTOT_cpiu_2010_bucket",
+    "us_born",
+    "gender",
+    "tenure",
+    "cpuma"
+  )
+  
+  output <- reg_data |>
+    split_term_column(varnames = varnames_dict) |>
+    # TODO: add unit test
+    add_intercept_v2(
+      variable = "RACE_ETH_bucket", # Variable to draw intercept from
+      reference_value = "White", # value of variable that will become intercept
+      coef_col = "coef_2000",
+      se_col = "coef_2000_se"
+    ) |>
+    complete_implicit_zeros(
+      adjust_by = adjust_by,
+      coef_col = "estimate",
+      se_col = NULL
+    ) 
+  
+  # TODO: this is incomplete and doesn't work. resume work on it
+  return(output)
+}
