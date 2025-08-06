fe38008d53f0f9790445c57dc96828b25d177858
lorae <stojanovic.lorae@gmail.com>
2025-08-06 11:05:05 -0400

    fix(fig02): remove undefined objects and begin refactor

--- src/figures/linear-reg/fig02-pctchg-hhsize-age-race.R
+++ src/figures/linear-reg/fig02-pctchg-hhsize-age-race.R
@@ -1,6 +1,106 @@
-##############################################
-#vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv# Figure 2
-##############################################
+# This script produces Figure 2 
+# 
+# 
+# The purpose of this script is to produce fast facts that are used in the draft 
+# version of this paper. 
+# Last modified mid-March 2025.
+
+# ----- Step 0: Load required packages ----- #
+library("dplyr")
+library("duckdb")
+library("stringr")
+library("tidyr")
+library("purrr")
+library("glue")
+library("readxl")
+library("ggplot2")
+library("base64enc")
+library("patchwork")
+library("sf")
+
+# ----- Step 1: Source helper functions ----- #
+
+devtools::load_all("../dataduck")
+
+# ----- Step 2: Import and wrangle data ----- #
+
+con <- dbConnect(duckdb::duckdb(), "data/db/ipums.duckdb")
+ipums_db <- tbl(con, "ipums_processed") |>
+  filter(GQ %in% c(0,1,2))
+
+# ----- Step 3: Define functions for tabulating summaries ----- #
+source("src/utils/aggregation-tools.R")
+
+
+
+### # FIGURE 1: Create the bar plot with side-by-side bars for 2000 and 2019
+# Create data for bar plot
+race_summary_2000 <- tabulate_summary(data = ipums_db, year = 2000, group_by = "RACE_ETH_bucket") |> mutate(year = 2000)
+race_summary_2019 <- tabulate_summary(data = ipums_db, year = 2019, group_by = "RACE_ETH_bucket") |> mutate(year = 2019)
+race_summary <- union_all(race_summary_2000, race_summary_2019) # Row bind the tables
+
+# Define a single main color for all bars
+main_color <- "steelblue"
+
+# Generate the plot
+fig01 <- ggplot(race_summary, aes(x = subgroup, y = hhsize, fill = factor(year))) +
+  geom_bar(stat = "identity", position = position_dodge(width = 0.8), 
+           width = 0.8, color = "black") +  # Bar border
+  geom_text(aes(label = round(hhsize, 2), group = year), 
+            position = position_dodge(width = 0.8), 
+            vjust = -0.5, size = 3) +  
+  scale_fill_manual(
+    values = c("2000" = alpha(main_color, 0.4), "2019" = alpha(main_color, 0.8)), 
+    name = "") +  # Ensures the legend colors match bar colors
+  labs(y = "Average Household Size") +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 0, hjust = 0.5),
+    legend.position = "bottom",
+    legend.box = "horizontal",
+    axis.title.x = element_blank(),
+    plot.margin = margin(t = 10, r = 10, b = 0, l = 10)
+  )
+
+# Save the plot
+#ggsave("results/fig01.png", plot = fig01, width = 6.5, height = 3.5, dpi = 500)
+
+# ----- Step 4c: RESULTS - Household size in 2000 and 2019 by age ----- #
+# TODO: export as table to Shiny app
+age_summary <- tabulate_summary_2year(data = ipums_db, years = c(2000,2019), group_by = "AGE")
+
+# Ages with the largest household sizes
+# TODO: highlight these rows in the Shiny app table
+age_summary |> slice_max(hhsize_2000, n = 1)
+age_summary |> slice_max(hhsize_2019, n = 1)
+
+# ----- Step 4d: RESULTS - Household size in 2000 and 2019 by age bucket ----- #
+# Define ordered levels for AGE_bucket
+# Note, this is brittle, since age buckets may change
+# TODO: repo-wide factor dictionary, updated upon initial data ingestion
+age_bucket_levels <- c("0-4", "5-9", "10-14", "15-19", "20-24", 
+                       "25-29", "30-34", "35-39", "40-44", "45-49", 
+                       "50-54", "55-59", "60-64", "65-69", "70-74", 
+                       "75-79", "80-84", "85plus")
+
+# Create named vector for group_encoding
+group_encoding_age_bucket <- setNames(age_bucket_levels, age_bucket_levels)
+
+# Use `tabulate_summary_2year()` with group_encoding to enforce natural order of age
+# buckets and generate data for figure 2
+# Define race/ethnicity categories, including "All"
+race_ethnicities <- c("All", "AAPI", "AIAN", "Black", "Hispanic", "White", "Multiracial", "Other")
+# Generate summary for all categories in a loop (with 5 cores, takes about 10 seconds)
+age_bucket_summary <- map_dfr(race_ethnicities, function(race) {
+  data_filtered <- if (race == "All") ipums_db else ipums_db |> filter(RACE_ETH_bucket == race)
+  
+  tabulate_summary_2year(
+    data = data_filtered,
+    years = c(2000, 2019),
+    group_by = "AGE_bucket",
+    group_encoding = group_encoding_age_bucket
+  ) |> mutate(RACE_ETH_bucket = race) # Add race/ethnicity label
+})
 # Define race order (ensuring correct factor levels for consistent ordering)
 race_order <- c("All", "AAPI", "AIAN", "Black", "Hispanic", "White", "Multiracial", "Other")
 age_bucket_summary <- age_bucket_summary %>%
@@ -47,7 +147,6 @@ left_plot <- ggplot(left_data, aes(x = subgroup, y = hhsize_pctchg_2000_2019,
   ) +
   labs(y = NULL, x = NULL)  # Remove y-axis label
 
-left_plot
 
 # Right plot (Facet titles on the RIGHT)
 right_plot <- ggplot(right_data, aes(x = subgroup, 
@@ -93,7 +192,6 @@ right_plot <- ggplot(right_data, aes(x = subgroup,
   ) +
   labs(y = NULL, x = NULL)  # Remove y-axis label
 
-right_plot
 
 # Combine both plots into one, side-by-side
 combined_plot <- left_plot + right_plot + 
@@ -102,54 +200,8 @@ combined_plot <- left_plot + right_plot +
     caption = "*The 80-84 age group's increase in the 'Other' category exceeds the y-axis limits of ±15%."
   )
 
-# Save the combined plot
-ggsave("results/fig02.png", plot = combined_plot, width = 6.5, height = 6.5, dpi = 500)
 
 # Display the combined plot
 print(combined_plot)
-##############################################
-#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
-##############################################
-
-##############################################
-#vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv# Modified Figure 2 (Figure 2a, 2b)
-##############################################
-
-race_order <- c("All", "AAPI", "AIAN", "Black", "Hispanic", "White", "Multiracial", "Other")
-age_bucket_summary <- age_bucket_summary |>
-  mutate(RACE_ETH_bucket = factor(RACE_ETH_bucket, levels = race_order))
-
-# # Split data into two halves
-# left_data <- age_bucket_summary %>% filter(RACE_ETH_bucket %in% race_order[1:4])  # First 4 races
-# right_data <- age_bucket_summary %>% filter(RACE_ETH_bucket %in% race_order[5:8])  # Last 4 races
-
-# Left plot (Facet titles on the LEFT)
-fig02a <- ggplot(age_bucket_summary |> filter(RACE_ETH_bucket == "All"), aes(x = subgroup, y = hhsize_pctchg_2000_2019, 
-                                                                             fill = RACE_ETH_bucket == "All")) +  # Conditional fill
-  geom_bar(stat = "identity") +
-  # facet_grid(rows = vars(RACE_ETH_bucket), scales = "fixed", switch = "y") +
-  geom_vline(aes(xintercept = as.numeric(subgroup)), color = "grey80", linetype = "dashed", size = 0.3) +
-  geom_hline(yintercept = 0, color = "black", size = 0.6) +
-  scale_fill_manual(values = c("TRUE" = "grey60", "FALSE" = "steelblue"), guide = "none") +  # Define colors
-  theme_minimal() +
-  theme(
-    strip.text.y.left = element_text(angle = 0, hjust = 1, vjust = 0.5, color = "black", size = 8),  # Labels on the left
-    strip.placement = "outside",
-    panel.spacing = unit(1, "lines"),
-    panel.spacing.y = unit(0.2, "lines"),
-    axis.text.x = element_text(angle = 30, hjust = 1, vjust = 1, size = 8),
-    
-    # Add subtle tick marks on the right side without grid lines
-    axis.text.y.right = element_text(size = 8, color = "black", hjust = 1),  # Right-align text
-    axis.ticks.y.right = element_line(color = "black", size = 0.3),  # Subtle tick marks
-    
-    # Remove major y-grid lines
-    panel.grid.major.y = element_blank(),
-    panel.grid.minor.y = element_blank(),
-    panel.grid.major.x = element_blank()
-  ) +
-  scale_y_continuous(labels = function(x) paste0(x, "%")) +
-  labs(y = NULL, x = NULL)  # Remove y-axis label
-
-fig02a
-ggsave("results/fig02a.png", plot = fig02a, width = 6.5, height = 3, dpi = 500)
+# Save the combined plot
+#ggsave("results/fig02.png", plot = combined_plot, width = 6.5, height = 6.5, dpi = 500)
