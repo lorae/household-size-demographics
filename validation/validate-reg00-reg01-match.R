12808df15e566f1e974b357e2141107bfa998c5e
lorae <stojanovic.lorae@gmail.com>
2025-08-08 11:47:47 -0400

    refactor(validate): DRY out reg00-reg01 comparisons

--- validation/validate-reg00-reg01-match.R
+++ validation/validate-reg00-reg01-match.R
@@ -48,63 +48,83 @@ read_coefs_2019 <- function(path, adjust_by) {
   return(output)
 }
 
-# Props 2000 ----
-cat("Comparing props 2000:\n")
-prop_2000_00_raw <- readRDS("throughput/reg00/props00_2000.rds")
-prop_2000_00 <- purrr::map_dfr(prop_2000_00_raw, extract_prop, year = 2000)
-prop_2000_01_raw <- readRDS("throughput/reg01/2000_prop.rds")
-prop_2000_01 <- purrr::map_dfr(prop_2000_01_raw, extract_prop, year = 2000)
-cat("Result:", all.equal(prop_2000_01, prop_2000_00, tolerance = 1e-6), "\n\n")
+# Comparison helpers -----
+compare_props <- function(year, reg00_path, reg01_path, tol = 1e-6) {
+  r00 <- readRDS(reg00_path) |> purrr::map_dfr(extract_prop, year = year)
+  r01 <- readRDS(reg01_path) |> purrr::map_dfr(extract_prop, year = year)
+  res <- all.equal(r01, r00, tolerance = tol)
+  tibble::tibble(kind = "props",
+                 year = year,
+                 ok = isTRUE(res),
+                 detail = list(res))  # list-col for the all.equal output
+}
 
-# Props 2019 ----
-cat("Comparing props 2019:\n")
-prop_2019_00_raw <- readRDS("throughput/reg00/props00_2019.rds")
-prop_2019_00 <- purrr::map_dfr(prop_2019_00_raw, extract_prop, year = 2019)
-prop_2019_01_raw <- readRDS("throughput/reg01/2019_prop.rds")
-prop_2019_01 <- purrr::map_dfr(prop_2019_01_raw, extract_prop, year = 2019)
-cat("Result:", all.equal(prop_2019_01, prop_2019_00, tolerance = 1e-6), "\n\n")
+read_coefs <- function(path, year) {
+  out <- readRDS(path)
+  if (year == 2000) {
+    out |>
+      dplyr::select(term, estimate, std.error) |>
+      dplyr::rename(!!paste0("coef_", year) := estimate,
+                    !!paste0("coef_", year, "_se") := std.error)
+  } else if (year == 2019) {
+    out |>
+      dplyr::rename(!!paste0("coef_", year) := estimate,
+                    !!paste0("coef_", year, "_se") := se_estimate)
+  } else stop("Unsupported year")
+}
 
-# Bedroom 2000 ----
-cat("Comparing bedroom 2000:\n")
-bedroom_2000_00 <- read_coefs_2000("throughput/reg00/model00_2000_bedroom_summary.rds")
-bedroom_2000_01 <- read_coefs_2000("throughput/reg01/2000_b.rds")
-bedroom_2000_00_adj <- bedroom_2000_00 |>
-  split_term_column() |>
-  add_intercept_v2(
-    variable = "RACE_ETH_bucket",
-    reference_value = "AAPI",
-    coef_col = "coef_2000",
-    se_col = "coef_2000_se"
-  ) |>
-  arrange(term)
-bedroom_2000_01_adj <- bedroom_2000_01 |>
-  split_term_column() |>
-  arrange(term)
+compare_coefs <- function(year, reg00_path, reg01_path, intercept_var, ref_level, tol = 1e-6) {
+  coef_col <- paste0("coef_", year)
+  se_col   <- paste0("coef_", year, "_se")
+  
+  r00 <- read_coefs(reg00_path, year) |>
+    split_term_column() |>
+    add_intercept_v2(variable = intercept_var,
+                     reference_value = ref_level,
+                     coef_col = coef_col,
+                     se_col   = se_col) |>
+    dplyr::arrange(term)
+  
+  r01 <- read_coefs(reg01_path, year) |>
+    split_term_column() |>
+    dplyr::arrange(term)
+  
+  res <- all.equal(r01, r00, tolerance = tol)
+  tibble::tibble(kind = "coefs",
+                 year = year,
+                 ok = isTRUE(res),
+                 detail = list(res),
+                 peek_r00 = list(r00 |> dplyr::filter(variable %in% c(intercept_var, "(Intercept)"))),
+                 peek_r01 = list(r01 |> dplyr::filter(variable %in% c(intercept_var, "(Intercept)"))))
+}
 
-bedroom_2000_00_adj |> filter(variable == "RACE_ETH_bucket" | variable == "(Intercept)") 
-bedroom_2000_01_adj |> filter(variable == "RACE_ETH_bucket" | variable == "(Intercept)") 
 
-cat("Result:", all.equal(bedroom_2000_01_adj, bedroom_2000_00_adj, tolerance = 1e-6), "\n\n")
+# PROPS
+prop_cfg <- tribble(
+  ~year, ~reg00_path,                               ~reg01_path,
+  2000,  "throughput/reg00/props00_2000.rds",       "throughput/reg01/2000_prop.rds",
+  2019,  "throughput/reg00/props00_2019.rds",       "throughput/reg01/2019_prop.rds"
+)
 
-# Bedroom 2019 ----
-cat("Comparing bedroom 2019:\n")
-bedroom_2019_00 <- read_coefs_2019("throughput/reg00/model00_2019_bedroom_summary-v5.rds")
-bedroom_2019_01 <- read_coefs_2019("throughput/reg01/2019_b.rds")
-bedroom_2019_00_adj <- bedroom_2019_00 |>
-  split_term_column() |>
-  add_intercept_v2(
-    variable = "RACE_ETH_bucket",
-    reference_value = "AAPI",
-    coef_col = "coef_2019",
-    se_col = "coef_2019_se"
-  ) |>
-  arrange(term)
-bedroom_2019_01_adj <- bedroom_2019_01 |>
-  split_term_column() |>
-  arrange(term)
+prop_results <- prop_cfg |>
+  pmap(compare_props)
 
-bedroom_2019_00_adj |> filter(variable == "RACE_ETH_bucket" | variable == "(Intercept)") 
-bedroom_2019_01_adj |> filter(variable == "RACE_ETH_bucket" | variable == "(Intercept)") 
+print(prop_results)
 
-cat("Result:", all.equal(bedroom_2019_01_adj, bedroom_2019_00_adj, tolerance = 1e-6), "\n\n")
+# COEFS (bedroom now; add more measures later)
+coef_cfg <- tribble(
+  ~measure,   ~year, ~reg00_path,                                         ~reg01_path,                 ~intercept_var,     ~ref_level,
+  "bedroom",  2000,  "throughput/reg00/model00_2000_bedroom_summary.rds", "throughput/reg01/2000_b.rds", "RACE_ETH_bucket", "AAPI",
+  "bedroom",  2019,  "throughput/reg00/model00_2019_bedroom_summary-v5.rds","throughput/reg01/2019_b.rds","RACE_ETH_bucket", "AAPI"
+  # add rows for "ppr", "ppbr", "persons", "rooms" w/ the correct paths
+)
+
+coef_results <- coef_cfg |>
+  mutate(res = pmap(list(year, reg00_path, reg01_path, intercept_var, ref_level),
+                    ~ compare_coefs(..1, ..2, ..3, ..4, ..5))) |>
+  mutate(ok = map_lgl(res, "ok"),
+         detail = map(res, "detail")) |>
+  select(-res)
+
+print(coef_results)
 
